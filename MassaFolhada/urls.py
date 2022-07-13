2d6a00e54e960e78859fa3ddf79e9ec3af585cea
GeraldaCalixto08 <106275241+GeraldaCalixto08@users.noreply.github.com>
2022-07-13 15:13:02 -0300

    att 13 do 07

--- MassaFolhada/urls.py
+++ MassaFolhada/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('base/', include('base.urls')),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from django.views.generic import TemplateView
 # Create your views here.
+
+'''
+def home(request):
+    return render('home.html')
+'''
+class HomeView(templateView):
+    template_name = 'home.html'
