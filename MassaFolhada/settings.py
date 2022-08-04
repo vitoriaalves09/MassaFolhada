add81cbb9a6fa09d7bbc7be43bbe0ff7765c4cf6
LucyelleFreitas <106275223+LucyelleFreitas@users.noreply.github.com>
2022-08-04 16:21:26 -0300

    dia 04.08
    atualizada algumas coisas

--- MassaFolhada/settings.py
+++ MassaFolhada/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
-]
+    'base',
+    'bootstrap5',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
