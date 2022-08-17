de9925deccf5e9c31ccc0d5fb0fc3ea09363d4f3
GeraldaCalixto08 <106275241+GeraldaCalixto08@users.noreply.github.com>
2022-08-17 17:54:04 -0300

    17/08
    atualizando

--- MassaFolhada/settings.py
+++ MassaFolhada/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
     'bootstrap5',
+    'base',
+
 ]
 
 MIDDLEWARE = [
