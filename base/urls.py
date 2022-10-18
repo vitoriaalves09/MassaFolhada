ac89f122c538559711e7a25467f1b2599d8ec42f
GeraldaCalixto08 <106275241+GeraldaCalixto08@users.noreply.github.com>
2022-10-18 13:03:34 -0300

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import HomeView, LoginView, AboutView, MenuView, InformationView, PaymentView, TableView, ProcedureView, FinishingView, PersonalDataView 
 
-
 urlpatterns = [
     path('start/', HomeView.as_view(), name='start'),
     path('login/', LoginView.as_view(), name='login'),
